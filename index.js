bdeb1e9600166f22e47b690daa41130363cd1632
Mokhamed <gari142@mail.ru>
2023-09-11 14:48:35 +0300

    Сделал добавление фото

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
+const morgan = require("morgan")
+
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"))
 app.use(require("./routes/users.route"));
 app.use(require("./routes/reviews.route"))
+app.use('/assets', express.static(__dirname + '/assets'))
+
 
 mongoose
   .connect(`${process.env.MONGO}CarInRent`)
