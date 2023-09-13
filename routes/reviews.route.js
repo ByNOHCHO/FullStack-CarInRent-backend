9d48130c2d3d8b5fd3a8c1da9b39551a70a22a64
talhache <adam_ibragimov_1996@mail.ru>
2023-09-13 10:33:55 +0300

    Fix routes reviews

--- routes/reviews.route.js
+++ routes/reviews.route.js
@@ -1,11 +1,13 @@
 const { Router } = require("express")
 const { reviewsController } = require("../controllers/reviews.controller")
+const { checkAuth } = require("../models/middlewares/auth.middleware")
+
 
 const router = Router()
 
-router.post("/reviews", reviewsController.addReviews)
-router.delete('/reviews/:id', reviewsController.deleteReviewsById)
+router.post('/reviews', checkAuth, reviewsController.addReviews)
+router.delete('/reviews/:id', checkAuth, reviewsController.deleteReviewsById)
 router.get('/reviews',  reviewsController.getReviews)
-router.patch('/reviews/:id', reviewsController.updateReviewsById)
+router.patch('/reviews/:id', checkAuth, reviewsController.updateReviewsById)
 
 module.exports = router
